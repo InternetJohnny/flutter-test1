4b0b563e03c1f036eb8f5378ae379a9a94ef0750
InternetJohnny <joaoandre6@gmail.com>
2020-01-13 09:51:04 +0000

    imported project

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/open_webpage.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+//import 'open_webpage.dart';
+
+const MaterialColor white = const MaterialColor(
+  0xFFFFFFFF,
+  const <int, Color>{
+    50: const Color(0xFFFFFFFF),
+    100: const Color(0xFFFFFFFF),
+    200: const Color(0xFFFFFFFF),
+    300: const Color(0xFFFFFFFF),
+    400: const Color(0xFFFFFFFF),
+    500: const Color(0xFFFFFFFF),
+    600: const Color(0xFFFFFFFF),
+    700: const Color(0xFFFFFFFF),
+    800: const Color(0xFFFFFFFF),
+    900: const Color(0xFFFFFFFF),
+  },
+);
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        //primarySwatch: Colors.blue,
+        primarySwatch: white,
+      ),
+      //home: WebViewWebPage(url: "https://www.appsource.xyz"),
+      home: OpenWebPageUrl(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
---
+++ lib/open_webpage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:flutter/services.dart';
+
+class OpenWebPageUrl extends StatelessWidget {
+  TextEditingController _textController =
+  TextEditingController(text: 'https://');
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+    ),
+    child: Scaffold(
+      appBar: AppBar(title: Text("AppSource")),
+      body: WebViewWebPage(url: "https://www.appsource.xyz"),
+    )
+    );
+  }
+}
+
+class WebViewWebPage extends StatelessWidget {
+  final String url;
+
+  WebViewWebPage({this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      url: url,
+      hidden: true,
+      //appBar: AppBar(title: Text("Open Web Page URL in webview")),
+    );
+  }
+}
